a1b90ee5584ea09251dd8f87b265cc0fc33d9c0b
Ubuntu <ubuntu@ip-10-0-0-211.eu-west-1.compute.internal>
2019-09-30 20:49:24 +0000

    rework on bet create UI

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   devServer: {
-    host: 'ec2-54-229-21-184.eu-west-1.compute.amazonaws.com',
+    host: 'ec2-54-171-252-27.eu-west-1.compute.amazonaws.com',
     port: 8080,
     disableHostCheck: true
   },
